cb66c0b7213e7755626149a52c323f20295bf962
Dileep Donepudi <donepudi4u@gmail.com>
2020-01-29 17:14:14 -0600

    initial commit

---
+++ src/main/java/org/tsn/app/admin/dto/MeetingDTO.java
@@ -0,0 +1,44 @@
+package org.tsn.app.admin.dto;
+
+import java.util.Date;
+
+public class MeetingDTO {
+
+	private Date meetingStartDateTime;
+	private Date meetingEndDateTime;
+	private String subject;
+	private String meetingAgenda;
+
+	public Date getMeetingStartDateTime() {
+		return meetingStartDateTime;
+	}
+
+	public void setMeetingStartDateTime(Date meetingStartDateTime) {
+		this.meetingStartDateTime = meetingStartDateTime;
+	}
+
+	public Date getMeetingEndDateTime() {
+		return meetingEndDateTime;
+	}
+
+	public void setMeetingEndDateTime(Date meetingEndDateTime) {
+		this.meetingEndDateTime = meetingEndDateTime;
+	}
+
+	public String getSubject() {
+		return subject;
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	public String getMeetingAgenda() {
+		return meetingAgenda;
+	}
+
+	public void setMeetingAgenda(String meetingAgenda) {
+		this.meetingAgenda = meetingAgenda;
+	}
+
+}
---
+++ src/test/java/org/tsn/tsnadmin/TsnAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tsn.tsnadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TsnAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
